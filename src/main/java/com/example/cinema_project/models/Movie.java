5f57ec95ada2cf75b534e835515db9a1fb39f504
Yongran <maiyongran@gmail.com>
2022-09-12 14:34:39 +0100

    create Movie class

--- src/main/java/com/example/cinema_project/models/Movie.java
+++ src/main/java/com/example/cinema_project/models/Movie.java
@@ -1,4 +1,63 @@
 package com.example.cinema_project.models;
 
 public class Movie {
+    private String title;
+
+    private long id;
+
+    private int length;
+
+    private int releaseDate;
+
+    private String genre;
+
+    public Movie(String title, int length, int releaseDate, String genre) {
+        this.title = title;
+        this.length = length;
+        this.releaseDate = releaseDate;
+        this.genre = genre;
+    }
+
+    public Movie() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
