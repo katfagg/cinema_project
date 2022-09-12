8d0c90d39a5a5589274f3463b6929476d7b94dca
Kat Fagg <katjfagg@gmail.com>
2022-09-12 14:58:20 +0100

    cinema classes populated

---
+++ src/main/java/com/example/cinema_project/controllers/CinemaController.java
@@ -0,0 +1,46 @@
+package com.example.cinema_project.controllers;
+
+
+import com.example.cinema_project.models.Cinema;
+import com.example.cinema_project.models.Movie;
+import com.example.cinema_project.models.Screen;
+import com.example.cinema_project.services.CinemaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies and screens")
+public class CinemaController {
+
+    @Autowired
+    CinemaService cinemaService
+
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        List<Movie> movies = cinemaService.getAllMovies();
+        return new ResponseEntity<>(movies, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity cancelMovie(@PathVariable long id){
+        CinemaService.deleteMovie(id);
+        return new ResponseEntity(null, HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping
+    public ResponseEntity<Movie> addCustomerToMovie(@RequestBody Movie movie){
+        Movie savedMovie = cinemaService.addCustomerToMovie(customer);
+        return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
+    }
+
+    @PostMapping
+    public ResponseEntity<Movie> addScreenToCinema(@RequestBody Cinema cinema){
+        Screen savedScreen = cinemaService.addScreenToCinema(screen);
+        return new ResponseEntity<>(savedScreen, HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/example/cinema_project/services/CinemaService.java
@@ -0,0 +1,50 @@
+package com.example.cinema_project.services;
+
+import com.example.cinema_project.models.Movie;
+import com.example.cinema_project.models.Screen;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CinemaService {
+    @Autowired
+    MovieRepository movieRepository;
+    @Autowired
+    ScreenRepository screenRepository;
+
+    public List<Movie> getAllMovies(){
+        return movieRepository.findAll();
+    }
+
+    public List<Movie> addNewMovie(Movie movie){
+        movieRepository.save(movie);
+        return movie;
+    }
+
+    public List<Movie> cancelMovie(long id){
+        movieRepository.deleteById(id);
+    }
+
+    public List<Screen> addNewScreen(Screen screen){
+        screenRepository.save(screen);
+        return screen;
+    }
+
+    public Screen addMovieToScreen(long screenId, long movieId){
+        Screen screen = screenRepository.findById(screenId).get();
+        Movie movie = movieService.getMovieById(movieId);
+        List<Movies> movies = screen.getMovies();
+        movies.add(movie);
+        screen.setMovies(movies);
+        screenRepository.save(movie);
+        return screen;
+    }
+
+    public List<Screen> removeScreen(long id){
+        screenRepository.deleteById(id);
+    }
+
+
+}
