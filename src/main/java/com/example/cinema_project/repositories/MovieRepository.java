bcb856447ba815405ae80e9bbab6a8eb996c39f6
Yongran <maiyongran@gmail.com>
2022-09-12 15:35:30 +0100

    modify repositories

--- src/main/java/com/example/cinema_project/repositories/MovieRepository.java
+++ src/main/java/com/example/cinema_project/repositories/MovieRepository.java
@@ -1,4 +1,7 @@
 package com.example.cinema_project.repositories;
 
-public interface MovieRepository {
+import com.example.cinema_project.models.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
--- src/main/java/com/example/cinema_project/repositories/ScreenRepository.java
+++ src/main/java/com/example/cinema_project/repositories/ScreenRepository.java
@@ -1,4 +1,7 @@
 package com.example.cinema_project.repositories;
 
-public interface ScreenRepository {
+import com.example.cinema_project.models.Screen;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScreenRepository extends JpaRepository<Screen, Long> {
 }
