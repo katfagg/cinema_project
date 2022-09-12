714f235f7c86fd9a9e680a8ff2201aebf5e1aeb4
Kat Fagg <katjfagg@gmail.com>
2022-09-12 15:14:27 +0100

    cinema class edited

--- src/main/java/com/example/cinema_project/models/Cinema.java
+++ src/main/java/com/example/cinema_project/models/Cinema.java
@@ -3,28 +3,11 @@ package com.example.cinema_project.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.List;
-@Entity(name = "cinema")
 public class Cinema {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
         private long id;
-        @Column
-        private String title;
-        @Column
-        private int length;
-        @Column
-        private int releaseDate;
-        @Column
-        private String genre;
-        @ManyToMany
-        @JoinTable(
-                name = "screenings",
-                joinColumns = {@JoinColumn(name = "movie_id", nullable = false)},
-                inverseJoinColumns = {@JoinColumn(name = "screen_id", nullable = false)}
-        )
-        @JsonIgnoreProperties({"movies"})
-        private List<Screen> screens;
+        private String branch;
+
 
         public Movie(String title, int length, int releaseDate, String genre) {
             this.title = title;
