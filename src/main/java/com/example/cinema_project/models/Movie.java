4a6f246d33c52def26743019a466649937623136
Yongran <maiyongran@gmail.com>
2022-09-12 14:56:51 +0100

    add entity for Movie

--- src/main/java/com/example/cinema_project/models/Movie.java
+++ src/main/java/com/example/cinema_project/models/Movie.java
@@ -1,15 +1,31 @@
 package com.example.cinema_project.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name = "movies")
 public class Movie {
-    private String title;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
+    @Column
+    private String title;
+    @Column
     private int length;
-
+    @Column
     private int releaseDate;
-
+    @Column
     private String genre;
+    @ManyToMany
+    @JoinTable(
+            name = "screenings",
+            joinColumns = {@JoinColumn(name = "movie_id", nullable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "screen_id", nullable = false)}
+    )
+    @JsonIgnoreProperties({"movies"})
+    private List<Screen> screens;
 
     public Movie(String title, int length, int releaseDate, String genre) {
         this.title = title;
