7c6608bb835c4a99a9b534e6c49c251bf30c4256
Guy Chalk <guychalk532@gmail.com>
2022-09-12 15:12:27 +0100

    guys changes

--- src/main/java/com/example/cinema_project/models/Customer.java
+++ src/main/java/com/example/cinema_project/models/Customer.java
@@ -1,4 +1,33 @@
 package com.example.cinema_project.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "customers")
 public class Customer {
+
+    private String name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long customerId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(long customerId) {
+        this.customerId = customerId;
+    }
+
 }
