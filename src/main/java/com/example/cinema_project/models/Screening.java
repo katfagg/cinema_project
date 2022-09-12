8c351ec867f0f8d995ca962a467c7da9ece9cd21
Yongran <maiyongran@gmail.com>
2022-09-12 17:25:19 +0100

    add entity for Screening

--- src/main/java/com/example/cinema_project/models/Screening.java
+++ src/main/java/com/example/cinema_project/models/Screening.java
@@ -1,16 +1,28 @@
 package com.example.cinema_project.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity(name = "screenings")
 public class Screening {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column
     private Movie movie;
-
+    @Column
     private Screen screen;
-
+    @ManyToMany
+    @JoinTable(
+            name = "screenings",
+            joinColumns = {@JoinColumn(name = "screening_id", nullable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "customer_id", nullable = false)}
+    )
+    @JsonIgnoreProperties({"screenings"})
     private List<Customer> customers;
 
     public Screening(Movie movie, Screen screen) {
