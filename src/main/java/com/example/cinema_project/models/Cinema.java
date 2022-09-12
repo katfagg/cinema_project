e95add88fb9ad1939ac2045a56ab29ec4031b6d9
Kat Fagg <katjfagg@gmail.com>
2022-09-12 15:12:37 +0100

    cinema class populated

--- src/main/java/com/example/cinema_project/models/Cinema.java
+++ src/main/java/com/example/cinema_project/models/Cinema.java
@@ -1,8 +1,35 @@
 package com.example.cinema_project.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+@Entity(name = "cinema")
 public class Cinema {
 
-    private String branch;
-    private Long id;
-    private
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+        @Column
+        private String title;
+        @Column
+        private int length;
+        @Column
+        private int releaseDate;
+        @Column
+        private String genre;
+        @ManyToMany
+        @JoinTable(
+                name = "screenings",
+                joinColumns = {@JoinColumn(name = "movie_id", nullable = false)},
+                inverseJoinColumns = {@JoinColumn(name = "screen_id", nullable = false)}
+        )
+        @JsonIgnoreProperties({"movies"})
+        private List<Screen> screens;
+
+        public Movie(String title, int length, int releaseDate, String genre) {
+            this.title = title;
+            this.length = length;
+            this.releaseDate = releaseDate;
+            this.genre = genre;
+        }
 }
---
+++ src/main/java/com/example/cinema_project/repositories/CustomerRepository.java
@@ -0,0 +1,4 @@
+package com.example.cinema_project.repositories;
+
+public interface CustomerRepository {
+}
