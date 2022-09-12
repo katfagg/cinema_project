e0a17a6fb39c5bf8858ac5368c1bd55fe3dc9afa
Yongran <maiyongran@gmail.com>
2022-09-12 17:41:50 +0100

    delete a repo

--- src/main/java/com/example/cinema_project/repositories/ScreeningRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cinema_project.repositories;
-
-import com.example.cinema_project.models.Screening;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScreeningRepository extends JpaRepository<Screening, Long> {
-}
