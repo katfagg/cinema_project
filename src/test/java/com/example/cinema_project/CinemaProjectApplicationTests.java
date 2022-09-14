34ca2498a40a6da7e680bbf391c7b69b8a68b7ad
Tariq-Hennache <tariq.hennache@gmail.com>
2022-09-14 11:53:40 +0100

    ff

--- src/test/java/com/example/cinema_project/CinemaProjectApplicationTests.java
+++ src/test/java/com/example/cinema_project/CinemaProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.cinema_project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CinemaProjectApplicationTests {
 
 	@Test
